b261be38222600841859d606c5c7fb2ce3f3f35a
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2023-08-23 19:10:49 -0300

    help

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gcoqueir <gcoqueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 09:33:47 by gcoqueir          #+#    #+#             */
-/*   Updated: 2023/08/22 13:35:53 by gcoqueir         ###   ########.fr       */
+/*   Updated: 2023/08/22 19:26:12 by gcoqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 int	main(int argc, char **argv)
 {
 	t_stack	stack;
-	int		i;
 
 	if (argc >= 2)
 	{
 		check_params(argc, argv);
-		stack.a = create_stack(argc, argv);
 		stack.copy = create_stack(argc, argv);
-		// insertion_sort(&stack.copy, argc - 1);
+		stack.a = create_stack(argc, argv);
+		// get_index(&stack.copy, argc - 1);
 	}
 	return (0);
 }
